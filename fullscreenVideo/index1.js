5de6cdc6b8d03bbc8005c607bf39d0fb78773a4e
viethung <viethung2308@gmail.com>
2023-05-26 20:25:36 +0700

    add fullscreen video

---
+++ fullscreenVideo/index1.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+	const videoIntroduce = document.createElement("div");
+	videoIntroduce.className = "container-fluid my-container h-100";
+	videoIntroduce.innerHTML = `
+	<div class="row h-100">
+	<div class="col-12">
+	<div class="video-wrapper">
+  <iframe id="headerVimeoID"  class="video-fullscreen embed-responsive-item w-100 h-100" src="https://player.vimeo.com/video/830524731?autoplay=1&controls=0&&muted=1&loop=0" frameborder="0" allow="autoplay"
+			allowfullscreen
+	</iframe>
+  </div>
+  </div>
+  </div>
+  `;
+	document.body.appendChild(videoIntroduce);
+
+	const iframe = document.getElementById("headerVimeoID");
+	const playerHeader = new Vimeo.Player(iframe);
+
+	playerHeader.on("ended", function () {
+		playerHeader
+			.pause()
+			.then(function () {
+				return playerHeader.getCurrentTime();
+			})
+			.then(function (seconds) {
+				playerHeader.setCurrentTime(seconds - 0.01);
+			});
+	});
+});
